5697befa0113a38280ace6d821c4e7de953fee04
Marco Rios <marco12y@outlook.com>
2023-08-30 11:19:21 -0300

    added middlewares.ts | using consign

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 const app = require('express')()
+const consign = require('consign')
+
 const PORT: number = 3001
+
+consign()
+    .then('./config/middlewares.ts')
+    .into(app)
 
 
 app.listen(PORT, () => {
